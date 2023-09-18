f43e76134ad402a80ff5aa6ecb87acfcdf960baf
merlou52 <61518160+merlou52@users.noreply.github.com>
2023-09-18 16:43:55 +0200

    setup SMUI theme

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+    <Button on:click={() => clicked++}>
+        <Label>Default</Label>
+    </Button>
+    <Button on:click={() => clicked++} disabled>
+        <Label>Disabled</Label>
+    </Button>
+    <Button on:click={() => clicked++} ripple={false}>
+        <Label>No Ripple</Label>
+    </Button>
+    <!--
+      Note: to target this class, you need to use
+      a :global() selector. See the styles below.
+    -->
+    <Button on:click={() => clicked++} class="myClass">
+        <Label>With a Class</Label>
+    </Button>
+</div>
+
+<pre class="status">Clicked: {clicked}</pre>
+
+<script lang="ts">
+    import Button, { Label } from '@smui/button';
+
+    let clicked = 0;
+</script>
+
+<style>
+    /* Accessing the class with "*" in front limits
+      the scope to anything under this component's
+      elements. */
+    * :global(.myClass) {
+        font-style: italic;
+    }
+</style>
+
